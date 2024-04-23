2da406f175327a27ce8eb0174dfcbc6221c593dc
FANTER-1 <147248849+FANTER-1@users.noreply.github.com>
2024-04-23 15:07:38 +0300

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,32 +1,8 @@
-"""
-URL configuration for Project999 project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path, include, re_path
-from .views import SubmitDataDetail, SubmitDataUpdate, SubmitDataList
+from django.urls import path, re_path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('submitData/<int:pk>/', SubmitDataDetail.as_view()),
-    path('submitData/<int:pk>/update/', SubmitDataUpdate.as_view()),
-    path('submitData/', SubmitDataList.as_view()),
-]
+from .views import SubmitDataListCreateAPIView, SubmitDataRetrieveUpdateDestroyAPIView
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -42,6 +18,8 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
+    path('submitData/', SubmitDataListCreateAPIView.as_view(), name='submit-data-list-create'),
+    path('submitData/<int:pk>/', SubmitDataRetrieveUpdateDestroyAPIView.as_view(), name='submit-data-detail'),
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
