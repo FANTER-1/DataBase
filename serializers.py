235069f6bcec89409e11f910f264fc18102a0240
FANTER-1 <147248849+FANTER-1@users.noreply.github.com>
2024-04-24 17:39:41 +0300

    Delete serializers.py

--- serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import SubmitData
-
-class SubmitDataSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = SubmitData
-        fields = '__all__'
